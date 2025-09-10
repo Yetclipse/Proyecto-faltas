e6dcb9a8834113f7e6f302e93d89cefeeb94f5c1
sebas <sebas@10.20.49.196>
2025-09-10 11:06:32 -0300

    subida 1

---
+++ src/main/java/CapaLogica/Proytecto_Faltas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package CapaLogica;
+
+/**
+ 
+ * @author Sebastien, Joaquin y Francisco
+ */
+public class Proytecto_Faltas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
